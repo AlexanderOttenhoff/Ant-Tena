ecf657df5c22ef6361978fba1d19e8f240c8a350
konikun <koni.kun@gmail.com>
2014-01-25 01:42:05 +0100

    Adding bandpass filters and control
    soundtest scene to test

---
+++ Assets/Scripts/AudioInputController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class FilterData
+{
+    public float HighPassCutoff;
+    public float LowPassCutoff;
+}
+
+public class AudioInputController : MonoBehaviour
+{
+    public BandPassFilter BandPass;
+    public string LeftButton;
+    public string RightButton;
+    public FilterData FilterLeftButton;
+    public FilterData FilterRightButton;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!BandPass)
+            enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool leftFilter = Input.GetButton(LeftButton);
+        bool rightFilter = Input.GetButton(RightButton);
+        if (leftFilter)
+        {
+            BandPass.ActivateFilter(FilterLeftButton.HighPassCutoff, FilterLeftButton.LowPassCutoff);
+        }else if (rightFilter)
+        {
+            BandPass.ActivateFilter(FilterRightButton.HighPassCutoff, FilterRightButton.LowPassCutoff);
+        }
+        else
+        {
+            BandPass.Deactivate(); // deactivate filters
+        }
+    }
+}
---
+++ Assets/Scripts/BandPassFilter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioLowPassFilter), typeof(AudioHighPassFilter))]
+public class BandPassFilter : MonoBehaviour
+{
+    public const float MaxFrequency = 22000.0f;
+    public const float MinFrequency = 0f;
+
+    public float HighPassCutoff;
+    public float LowPassCutoff;
+    public bool OffOnAwake;
+
+    private AudioLowPassFilter _lowPass;
+    private AudioHighPassFilter _highPass;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _lowPass = GetComponent<AudioLowPassFilter>();
+	    _highPass = GetComponent<AudioHighPassFilter>();
+	    if (OffOnAwake)
+	    {
+	        _lowPass.enabled = false;
+	        _highPass.enabled = false;
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _lowPass.cutoffFrequency = LowPassCutoff;
+	    _highPass.cutoffFrequency = HighPassCutoff;
+	}
+
+    /// <summary>
+    /// Activates a bandpass filter which allows frequencies between highPassCutoff and lowPassCutoff.
+    /// </summary>
+    public void ActivateFilter(float highPassCutoff, float lowPassCutoff)
+    {
+        LowPassCutoff = lowPassCutoff;
+        HighPassCutoff = highPassCutoff;
+        _lowPass.enabled = true;
+        _lowPass.enabled = true;
+    }
+
+    public void Deactivate()
+    {
+        _lowPass.enabled = false;
+        _lowPass.enabled = false;
+    }
+}
