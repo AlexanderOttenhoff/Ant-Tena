daeb252f8da248bb5188e4b59abed6c7c03685a3
Xantomen <xantomen@gmail.com>
2014-01-26 03:05:43 +0100

    Fixed CarSpawner Script

---
+++ Assets/Terrain_Assets/L2S2a_VehicleSpawnerScript.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+//Inspector Variables
+var vehicleCreationRate: float = 10;
+var vehicleSpeed: float = 10;
+
+var vehicleObject: Rigidbody;
+
+//Private variables
+
+private var countdown : int;
+
+
+function Start () {
+
+	countdown = vehicleCreationRate;
+
+}
+
+function Update () {
+
+	countdown -= Time.deltaTime;
+	
+	if(countdown <= 0)
+	{
+		
+		countdown = vehicleCreationRate;
+		
+		var newVehicle : Rigidbody = Instantiate(vehicleObject, transform.position, transform.rotation);
+		
+		newVehicle.velocity = transform.forward * vehicleSpeed;
+		
+		
+	}	
+
+
+}
