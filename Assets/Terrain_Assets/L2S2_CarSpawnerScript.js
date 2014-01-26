87f0bafd8afc67ab043a93defb0b355339a231b5
Xantomen <xantomen@gmail.com>
2014-01-26 01:56:16 +0100

    More Train Sounds and CarRespawnPrefab

---
+++ Assets/Terrain_Assets/L2S2_CarSpawnerScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+//Inspector Variables
+var carCreationRate: float = 10;
+var countdown : int;
+
+
+function Start () {
+
+	countdown = carCreationRate;
+
+}
+
+function Update () {
+
+	timer -= Time.deltaTime;
+	
+	if(timer <= 0)
+	{
+		
+		countdown = carCreationRate;
+		Instantiate(brick, Vector3 (x, y, 0), Quaternion.identity);
+		
+	}	
+
+
+}
